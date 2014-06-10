1a27ea95c2bfdd1fea44ae9c814ad0b92e395841
Vaibhav Omanwar <vaibhavomanwar@gmail.com>
2014-06-10 14:55:42 +0900

    Cached Matrix Inverse Calculation

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,40 @@
+## Following function cache the Inverse of a Matrix especially useful for 
+## computing the inverse of large matrices.
+## These functions illustrate the advantage of the scoping rules of the R 
+## language and demonstrates how they can be manipulated to preserve state 
+## inside of an R object.
+
+## This function creates a special "matrix" object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
+}
